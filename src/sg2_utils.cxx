a4c31faabfc39fc172cb107d127725912df9dee1
Benoit Gschwind <benoit.gschwind@mines-paristech.fr>
2021-04-07 15:45:59 +0200

    Implement approx_Dpsi/epsilon/nu/M_0/deltat_msc

---
+++ src/sg2_utils.cxx
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Benoit Gschwind <benoit.gschwind@mines-paristech.fr>
+ *
+ * This file is part of libsg2.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "sg2_utils.hxx"
+
+
+namespace sg2 {
+
+sinusoidal_approx<1> approx_Dpsi = {
+		2444239.5, 0.0, 0.0,
+		{ 2.0*PI/6791.164405 },
+		{ 8.329092e-5 },
+		{ -2.052757 }
+};
+
+
+sinusoidal_approx<1> approx_epsilon = {
+		2444239.5, -6.216374e-9, 4.091383e-1,
+		{ 2.0*PI/6791.164405 },
+		{ 4.456183e-5 },
+		{ 2.660352 }
+};
+
+sinusoidal_approx<0> approx_nu0 = {
+		2444239.5, 6.300388099, 1.742079140,
+		{ },
+		{ },
+		{ }
+};
+
+sinusoidal_approx<0> approx_M_0 = {
+		2444239.5, 1.0/58.130099904, -1.399410798,
+		{ },
+		{ },
+		{ }
+};
+
+polynomial_approx<5, 6> approx_deltat_msc = {
+		{
+			{{1941,1961}, 1950, {0.0, 0.0, 1.0/2547.0, -1.0/233.0, 0.407, 29.07}},
+			{{1961,1986}, 1975, {0.0, 0.0, -1.0/718.0, -1.0/260.0, 1.067, 45.45}},
+			{{1986,2005}, 2000, {0.00002373599, 0.000651814, 0.0017275, -0.060374, 0.3345, 63.86}},
+			{{2005,2050}, 2000, {0.0, 0.0, 0.0, 0.005576068, 0.32100612, 62.8938127}},
+			{{2050,2150}, 2000, {0.0, 0.0, 0.0, 1.0/312.5, 1.7148, -0.74-0.1-0.01}}
+		},
+};
+
+
+} // namepsace sg2
+
---
+++ src/sg2_utils.hxx
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 Benoit Gschwind <benoit.gschwind@mines-paristech.fr>
+ *
+ * This file is part of libsg2.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_SG2_UTILS_HXX_
+#define SRC_SG2_UTILS_HXX_
+
+#include "sg2_typedef.hxx"
+#include "sg2_math.hxx"
+
+namespace sg2 {
+
+template<int const N>
+struct sinusoidal_approx {
+	double j0;
+	double a;
+	double b;
+	double a0[N];
+	double ro[N];
+	double phi[N];
+
+	double compute(double jd) const {
+		double jc = jd - j0;
+		double s = a*jc+b;
+		for (int kf = 0; kf < N; ++kf) {
+			s += ro[kf]*math::cos(a0[kf]*jc-phi[kf]);
+		}
+		return s;
+	}
+
+};
+
+template<int const nP, int const S>
+struct polynomial_approx {
+	struct {
+		int year_interval[2];
+		int year_offset;
+		double poly_coeff[S];
+	} data[nP];
+
+	double compute(int y) const {
+		if (y < data[0].year_interval[0])
+			return NAN;
+
+		int i;
+		for (i = 0;; ++i) {
+			if (i >= nP)
+				return NAN;
+			if (y>=data[i].year_interval[1])
+				break;
+		}
+
+		// Polyval
+		double const x = y-data[i].year_offset;
+		double s = 0.0;
+		for (int k = 0; k < S; ++k) {
+			s = s * x + data[i].poly_coeff[k];
+		}
+		return s;
+	}
+
+};
+
+extern sinusoidal_approx<1> approx_Dpsi;
+extern sinusoidal_approx<1> approx_epsilon;
+extern sinusoidal_approx<0> approx_nu0;
+extern sinusoidal_approx<0> approx_M_0;
+extern polynomial_approx<5, 6> approx_deltat_msc;
+
+} // namespace sg2
+
+
+#endif /* SRC_SG2_UTILS_HXX_ */
