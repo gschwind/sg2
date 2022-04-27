d1d2ecb3dd910a4536096f4446906ecabb83c51b
Philippe Blanc <philippe.blanc@mines-paristech.fr>
2022-04-27 18:22:06 +0200

    matlab: Implement ymdh_to_jd in native matlab

--- matlab/sg2_ymdh_to_jd.m
+++ matlab/sg2_ymdh_to_jd.m
@@ -1,11 +1,13 @@
+function jd = sg2_ymdh_to_jd(ymdh)
 % function jd = sg2_ymdh_to_jd(ymdh)
 %
-% Transformation of a 2-D array of size (M,4) (year month day hour) into
-% a 1-D array of size (M,1) of julian dates
+% Transformation of (N+1)-D array with elements (year month day hour)
+% into a N-D array of Julian Date (jd)
+%
 %
 % Input:
 %
-% ymdh: 2-D array of size (M,4) (y,m,d,h) where:
+% ymdh: (N+1)-D array of (n1, ..., nN, 4)  with (y,m,d,h) elements where:
 %        - y is an array of year number
 %        - m is an array of month number in [1,12]
 %        - d is an array of day of month in [1,31]
@@ -13,5 +15,27 @@
 %
 % Output:
 %
-% jd: 1-D array of double of size (M,1)
+% jd: N-D array of double of size (n1, ..., nN)
+
+shape = size(ymdh);
+if (shape(end)~=4)
+    error('The last size of ymdh should be 4');
+end
+
+n = prod(shape(1:end-1));
+ymdh = reshape(ymdh,[n 4]);
+
+jd = zeros(n,1);
+
+idx = find(ymdh(:,2)<3);
+ymdh(idx,2) = ymdh(idx,2)+12;
+ymdh(idx,1) = ymdh(idx,1)-1;
+
+jd = 1721028 + ymdh(:,3) + floor((153 * ymdh(:,2) - 2) / 5) + 365 * ymdh(:,1) + floor(ymdh(:,1) / 4) ...
+    - floor(ymdh(:,1) / 100) + floor(ymdh(:,1) / 400) + ymdh(:,4) / 24 - 0.5;
+
+jd = reshape(jd, [shape(1:end-1) 1]);
+
+
+
 
