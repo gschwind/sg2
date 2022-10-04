30232d1d76fa5dbe29a0e8dc6c07dff56ffde454
Benoit Gschwind <benoit.gschwind@mines-paristech.fr>
2022-10-04 18:24:12 +0200

    pysg2: Add long_description metadata

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
     license = 'GNU Lesser General Public License v3 (LGPLv3)',
     description = 'Solar Geometry 2 library',
     platforms=["Windows", "Linux"],
+    long_description="""Solar Geometry 2 (SG2) is the second generation of library for computing the relative position of the sun and the earth. Valid over the time period 1980-2100, the algorithm is 20 times faster than the well-know SPA algorithm, with an accuracy order of approx. 0.005°. Reference article: Blanc P. and L. Wald,  The SG2 algorithm for a fast and accurate computation of the position of the sun for multi-decadal time period. Solar Energy 88, 3072-3083, 2012, doi: 10.1016/j.solener.2012.07.018.""",
     ext_modules = [module]
 )
 
