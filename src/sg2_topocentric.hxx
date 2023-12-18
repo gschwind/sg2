ba95d95c0b52d178bbd3f91b04a100734fbaf374
Benoît Gschwind <benoit.gschwind@mines-paristech.fr>
2023-12-18 17:38:50 +0100

    Fix unused variable in topocentric_correction_refraction_SAE

--- src/sg2_topocentric.hxx
+++ src/sg2_topocentric.hxx
@@ -102,9 +102,6 @@ inline double topocentric_correction_refraction_SAE(double const gamma_S0, doubl
 	double const R = 0.029614018235657;
 	/*(tan(gamma_S0_seuil + 0.0031376 / (gamma_S0_seuil+ 0.089186))) */
 	double K;
-	double tan_gamma_S0 = 0.0;
-	double gamma_S0_2 = 0.0, gamma_S0_3 = 0.0, gamma_S0_4 = 0.0;
-	unsigned long k;
 
 	K = (P / 1010.0) * (283. / (273. + T)) * 2.96706e-4;
 
