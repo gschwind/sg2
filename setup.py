67e372a586581f66af7ebc12f76dac94dc7dd3cd
Benoît Gschwind <benoit.gschwind@mines-paristech.fr>
2024-08-19 11:55:42 +0200

    pysg2: fix setup.py to include all files required for sdist

--- setup.py
+++ setup.py
@@ -8,17 +8,34 @@ import numpy
 import os, sys, re
 
 params = {
-    'include_dirs': [numpy.get_include(), "third-parties/python-bind-helper", "src"],
+    'include_dirs': [
+        numpy.get_include(),
+        "third-parties/python-bind-helper",
+        "src"
+    ],
     'sources': [
         "python/pysg2.cxx",
         "src/sg2.cxx",
+        "src/sg2_data_handler.cxx",
         "src/sg2_date.cxx",
-        "src/sg2_utils.cxx",
         "src/sg2_geocentric.cxx",
+        "src/sg2_geocentric_data.cxx",
         "src/sg2_geopoint.cxx",
         "src/sg2_topocentric.cxx",
-        "src/sg2_data_handler.cxx",
-        "src/sg2_geocentric_data.cxx"
+        "src/sg2_utils.cxx"
+    ],
+    'depends': [
+        "src/sg2_data_handler.hxx",
+        "src/sg2_date.hxx",
+        "src/sg2_err.hxx",
+        "src/sg2_geocentric.hxx",
+        "src/sg2_geopoint.hxx",
+        "src/sg2.hxx",
+        "src/sg2_math.hxx",
+        "src/sg2_topocentric.hxx",
+        "src/sg2_typedef.hxx",
+        "src/sg2_utils.hxx",
+        "third-parties/python-bind-helper/python-bind-helper.hxx"
     ]
 }
 
